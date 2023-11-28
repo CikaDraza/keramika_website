c7b8dade4e9eb25e0ec06b8fe11787191336a158
CikaDraza <drazic.milan@gmail.com>
2023-11-28 19:28:07 +0100

    error youtube api

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ const YOUTUBE_PLAYLIST = process.env.YOUTUBE_PLAYLIST;
 const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY2;
 
  export async function getServerSideProps() {
-  const { data } = await axios(`${YOUTUBE_PLAYLIST}?part=snippet&playlistId=PLss-O45xpb4ePG5XbxL5uOAyqyvBk52Tl&key=${YOUTUBE_API_KEY}`);
+  const { data } = await axios.get(`${YOUTUBE_PLAYLIST}?part=snippet&playlistId=PLss-O45xpb4ePG5XbxL5uOAyqyvBk52Tl&key=${YOUTUBE_API_KEY}`);
 
   return {
     props: {
