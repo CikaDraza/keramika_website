99dc26aec9484ef687303f9b0972b81513527185
CikaDraza <drazic.milan@gmail.com>
2024-01-07 23:04:43 +0100

    vercel analytic

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.scss';
 import LayoutPage from '../src/LayoutPage';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <LayoutPage>
       <Component {...pageProps} />
+      <Analytics />
     </LayoutPage>
   )
 }
